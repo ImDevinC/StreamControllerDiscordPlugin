9480080bc6c0e98a6e6ff2eb8370b0d86fa8a090
Devin Collins <me@imdevinc.com>
2024-08-24 11:32:37 -0700

    Add requirements.txt

---
+++ __install__.py
@@ -0,0 +1,6 @@
+from streamcontroller_plugin_tools.installation_helpers import create_venv
+from os.path import join, abspath, dirname
+
+toplevel = dirname(abspath(__file__))
+create_venv(join(toplevel, ".venv"), join(
+    toplevel, "assets", "requirements.txt"))
