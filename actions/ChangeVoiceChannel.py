d84d954d9b3cf13d95e2f4896eac99872d904376
Devin Collins <me@imdevinc.com>
2025-09-27 20:45:48 -0700

    feat(voice): Add support for joining/leaving voice channels

--- actions/ChangeVoiceChannel.py
+++ actions/ChangeVoiceChannel.py
@@ -8,15 +8,41 @@ from src.backend.PluginManager.InputBases import Input
 
 from GtkHelper.GenerativeUI.EntryRow import EntryRow
 
+from ..discordrpc.commands import VOICE_CHANNEL_SELECT
+
 
 class Icons(StrEnum):
-    CHANGE_VOICE = ""
+    VOICE_CHANNEL_ACTIVE = "voice-active"
+    VOICE_CHANNEL_INACTIVE = "voice-inactive"
 
 
 class ChangeVoiceChannel(DiscordCore):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.has_configuration = True
+        self._current_channel: str = ""
+        self.icon_keys = [Icons.VOICE_CHANNEL_ACTIVE,
+                          Icons.VOICE_CHANNEL_INACTIVE]
+        self.current_icon = self.get_icon(Icons.VOICE_CHANNEL_INACTIVE)
+        self.icon_name = Icons.VOICE_CHANNEL_INACTIVE
+
+    def on_ready(self):
+        super().on_ready()
+        self.plugin_base.add_callback(
+            VOICE_CHANNEL_SELECT, self._update_display)
+        self.backend.register_callback(
+            VOICE_CHANNEL_SELECT, self._update_display)
+
+    def _update_display(self, value: dict):
+        if not self.backend:
+            self.show_error()
+            return
+        self.hide_error()
+        self._current_channel = value.get(
+            "channel_id", None) if value else None
+        self.icon_name = Icons.VOICE_CHANNEL_INACTIVE if self._current_channel is None else Icons.VOICE_CHANNEL_ACTIVE
+        self.current_icon = self.get_icon(self.icon_name)
+        self.display_icon()
 
     def create_generative_ui(self):
         self._channel_row = EntryRow(
@@ -42,6 +68,13 @@ class ChangeVoiceChannel(DiscordCore):
         )
 
     def _on_change_channel(self, _):
+        if self._current_channel is not None:
+            try:
+                self.backend.change_voice_channel(None)
+            except Exception as ex:
+                log.error(ex)
+                self.show_error(3)
+            return
         channel = self._channel_row.get_value()
         try:
             self.backend.change_voice_channel(channel)
